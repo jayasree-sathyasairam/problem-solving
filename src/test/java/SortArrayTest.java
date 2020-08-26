c86164f455dfb53442d318240781846dc8a52823
Jayasree Sathyasairam <jayasree-sathyasairam>
2020-08-26 19:53:58 +0530

    Written as seperate testcases for best practice

--- src/test/java/SortArrayTest.java
+++ src/test/java/SortArrayTest.java
@@ -15,92 +15,104 @@ public class SortArrayTest {
     }
 
     @Test
-    public void testInputConstruction(){
-        //testForSmallNumbers
+    public void shouldReturnIntegerArrayListForSmallNumbers(){
         assertEquals(sortArray.constructInput("1,2,3,4,5").toString(), "[1, 2, 3, 4, 5]");
+    }
 
-        //testForBigNumbers
+    @Test
+    public void shouldReturnIntegerArrayListForBigNumbers(){
         assertEquals(sortArray.constructInput("2147483647,2147483647,2147483647").toString(), "[2147483647, 2147483647, 2147483647]");
+    }
 
-        //testForEmptyString
+    @Test
+    public void shouldReturnNumberFormatExceptionForEmptyArray(){
         try{
             sortArray.constructInput("");
         }catch(NumberFormatException e){
             assertEquals(e.getClass(), NumberFormatException.class);
         }
+    }
 
-        //testForLongNumbers
+    @Test
+    public void shouldReturnNumberFormatExceptionForLongNumbers(){
         try{
             sortArray.constructInput("1,2,3676384296572647582364");
         }catch(NumberFormatException e){
             assertEquals(e.getClass(), NumberFormatException.class);
         }
+    }
 
-        //testForNegativeNumbers
-        try {
+    @Test
+    public void shouldReturnNumberFormatExceptionForNegativeNumbers(){
+        try{
             sortArray.constructInput("1,-2,3");
-        } catch (Exception e) {
-            assertEquals(e.getClass(), NumberFormatException.class);
-        }
-
-        //testForDecimalNumbers
-        try {
-            sortArray.constructInput("1,2.7,3");
-        } catch (Exception e) {
-            assertEquals(e.getClass(), NumberFormatException.class);
-        }
-
-        //testForAlphabets
-        try {
-            sortArray.constructInput("a,b,c");
-        } catch (Exception e) {
-            assertEquals(e.getClass(), NumberFormatException.class);
-        }
-
-        //testForSpecialCharacters
-        try {
-            sortArray.constructInput("$,#,@");
-        } catch (Exception e) {
-            assertEquals(e.getClass(), NumberFormatException.class);
-        }
-
-        //testForInputFormatWithSpaceBetweenNumbers
-        try {
-            sortArray.constructInput("1, 2, 3");
-        } catch (Exception e) {
+        }catch(NumberFormatException e){
             assertEquals(e.getClass(), NumberFormatException.class);
         }
     }
 
     @Test
-    public void testQuickSort(){
-        ArrayList<Integer> numArr;
+    public void shouldReturnNumberFormatExceptionForDecimalNumbers(){
+        try{
+            sortArray.constructInput("1,2.7,3");
+        }catch(NumberFormatException e){
+            assertEquals(e.getClass(), NumberFormatException.class);
+        }
+    }
 
-        //testForSingleDigitNumbers
+    @Test
+    public void shouldReturnNumberFormatExceptionForAlphabets(){
+        try{
+            sortArray.constructInput("a,b,c");
+        }catch(NumberFormatException e){
+            assertEquals(e.getClass(), NumberFormatException.class);
+        }
+    }
+
+    @Test
+    public void shouldReturnNumberFormatExceptionForSpecialCharacters(){
+        try{
+            sortArray.constructInput("$,#,@");
+        }catch(NumberFormatException e){
+            assertEquals(e.getClass(), NumberFormatException.class);
+        }
+    }
+
+    @Test
+    public void shouldReturnNumberFormatExceptionForInputFormatWithSpaceBetweenNumbers(){
+        try{
+            sortArray.constructInput("1, 2, 3");
+        }catch(NumberFormatException e){
+            assertEquals(e.getClass(), NumberFormatException.class);
+        }
+    }
+
+    @Test
+    public void shouldReturnLargestIntForSingleDigitNumbers(){
+        ArrayList<Integer> numArr;
         numArr = sortArray.constructInput("1,2,3,4,5");
         sortArray.quickSort(numArr, 0, numArr.size()-1);
         assertEquals(numArr.toString(), "[5, 4, 3, 2, 1]");
-
-        //testForVariableDigitNumbers
-        numArr = sortArray.constructInput("10,9,8,7,4,29,81");
-        sortArray.quickSort(numArr, 0, numArr.size()-1);
-        assertEquals(numArr.toString(), "[9, 8, 81, 7, 4, 29, 10]");
-
-        //testForSingletonArray
-        numArr = sortArray.constructInput("1");
-        sortArray.quickSort(numArr, 0, numArr.size()-1);
-        assertEquals(numArr.toString(), "[1]");
-
-        //testForBiggerNumbers
-        numArr = sortArray.constructInput("2147483643,2147483642,21474888");
-        sortArray.quickSort(numArr, 0, numArr.size()-1);
-        assertEquals(numArr.toString(), "[21474888, 2147483643, 2147483642]");
     }
 
     @Test
-    public void testPartition(){
+    public void shouldReturnLargestIntForVariableDigitNumbers(){
+        ArrayList<Integer> numArr;
+        numArr = sortArray.constructInput("10,9,8,7,4,29,81");
+        sortArray.quickSort(numArr, 0, numArr.size()-1);
+        assertEquals(numArr.toString(), "[9, 8, 81, 7, 4, 29, 10]");
+    }
 
-        //testPivotNumber
+    @Test
+    public void shouldReturnLargestIntForSingletonArray(){
+        ArrayList<Integer> numArr;
+        numArr = sortArray.constructInput("1");
+        sortArray.quickSort(numArr, 0, numArr.size()-1);
+        assertEquals(numArr.toString(), "[1]");
+    }
+
+    @Test
+    public void shouldReturnPivotNumberForGivenArray(){
         sortArray.quickSort(sortArray.constructInput("1,2,3,4,5"), 0, 4);
         assertEquals(0, 0);
     }
